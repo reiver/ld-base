2d2ae792e31b5190c23c37d3b11e607b5c876437
Charles Iliya Krempeaux <iamreiver@gmail.com>
2025-01-20 06:04:06 -0800

    initial code

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/reiver/ld-base/srv/log"
+)
+
+func main() {
+	log := logsrv.Prefix("main")
+
+	log.Inform("ld-base ⚡")
+	//tell()
+
+	//log.Inform("Here we go…")
+	//webserve()
+}
